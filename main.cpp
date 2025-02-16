c249cb72fc20711b4ccdbdc2adcac48d1723d9ba
tellSlater <liakou.metal@gmail.com>
2025-02-16 04:00:24 +0100

    sleep-bugged

--- main.cpp
+++ main.cpp
@@ -1,113 +1,72 @@
 /*
- * GccApplication1.cpp
+ * chest_tune.cpp
+ * 
  *
- * Created: 2025-02-15 19:46:40
- * Author : ilcha
+ * Chip used: ATTiny13A
+ * The internal oscillator and no prescaling is used for this project.
+ * The state of the chip's fuses should be (E:FF, H:FF, L:6A) when not locked.
+ *
+ *								 _________
+ * PIN1 - N/C       		   _|	 O    |_		PIN8 - VCC
+ * PIN2	- Note 2     		   _|		  |_		PIN7 - Note 1
+ * PIN3	- Note 3        	   _|ATTiny13A|_		PIN6 - Light sensor LDR
+ * PIN4	- Ground			   _|		  |_		PIN5 - Virtual GND
+ *							    |_________|
  */ 
 
 
-/*
-
-#define F_CPU   9600000
-#include <util/delay.h>
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
-
-#define C6_OCR 71  // OCR0A value for C6 (1046.5 Hz)
-#define DURATION_MS 3000  // 3 seconds duration
-
-volatile uint16_t timer_count = 0;
-volatile uint8_t play_note = 0;
-
-ISR(TIM0_COMPA_vect) {
-	if (play_note) {
-		if (timer_count < (DURATION_MS * 2)) { // 2 toggles per cycle
-			if (timer_count % 4 == 0) PORTB |= (1 << PINB2);  // 25% duty ON
-			else if (timer_count % 4 == 1) PORTB &= ~(1 << PINB2); // 25% duty OFF
-			timer_count++;
-			} else {
-			play_note = 0;  // Stop playing after 3 seconds
-			TIMSK0 &= ~(1 << OCIE0A); // Disable interrupt
-			PORTB &= ~(1 << PINB2); // Ensure output is LOW
-		}
-	}
-}
-
-void setup_timer0() {
-	TCCR0A = (1 << WGM01);  // CTC Mode
-	TCCR0B = (1 << CS01) | (1 << CS00); // Prescaler 64
-	OCR0A = C6_OCR;
-	TIMSK0 |= (1 << OCIE0A);  // Enable CTC interrupt
-}
-
-int main() {
-	DDRB |= (1 << PINB2);  // Set PINB2 as output
-	DDRB &= ~(1 << PINB1); // PINB1 as input
-	PORTB |= (1 << PINB1); // Enable pull-up on PINB1
-	sei(); // Enable global interrupts
-
-	while (1) {
-		if (!(PINB & (1 << PINB1))) { // Button press detected (active low)
-			_delay_ms(50); // Debounce
-			if (!(PINB & (1 << PINB1))) { // Confirm press
-				play_note = 1;
-				timer_count = 0;
-				setup_timer0();
-			}
-			while (!(PINB & (1 << PINB1))); // Wait for button release
-		}
-	}
-}
-
-*/
-
-
-/*
-
-#define F_CPU   9600000
-#include <avr/io.h>
-#include <util/delay.h>
-
-int main() {
-	DDRB |= (1 << PINB2);  // Set PB2 as output
-
-	while (1) {
-
-		
-		PORTB = (1 << PINB2);  // Toggle PB2
-		_delay_us(236);         // Adjust delay for different frequencies
-		PORTB = 0x00;  // Toggle PB2
-		_delay_us(707); 
-		
-		//PORTB = (1 << PINB2);  // Toggle PB2
-		//_delay_us(280);         // Adjust delay for different frequencies
-		//PORTB = 0x00;  // Toggle PB2
-		//_delay_us(842); 
-         // Adjust delay for different frequencies
-	}
-}
-
-*/
-
-
 #define F_CPU   9600000
 #include <util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 
 volatile uint16_t duration = 0;
 
 
-void setup_timer0() {
-	TCCR0B = (1 << CS00);  // Prescaler = 1 (Timer runs at full 9.6 MHz)
-	TIMSK0 = (1 << TOIE0); // Enable Timer0 Overflow Interrupt
-	sei();                 // Enable global interrupts
+void setup_pins(){
+	DDRB |= 1 << PINB0;	    // OUTPUT - Sets virtual ground
+	DDRB &= ~(1 << PINB1);  // INPUT  - Interrupt sensing LDR for wakeup
+	PORTB |= (1 << PINB1);  // Enable pull-up resistor on PB1
+	DDRB |= 1 << PINB2;	    // OUTPUT - Note 1
+	DDRB |= 1 << PINB3;	    // OUTPUT - Note 2
+	DDRB |= 1 << PINB4;	    // OUTPUT - Note 3
 }
 
 
-void quarter_square(const uint16_t period_samples, const uint16_t duration_samples, const uint8_t pin){
+void setup_sleep() {
+	MCUCR |= (1 << SM1) | (1 << SE);
+}
+
+
+void setup_timer0() {
+	TCCR0B = (1 << CS00);   // Prescaler = 1 (Timer runs at full 9.6 MHz)
+	TIMSK0 = (1 << TOIE0);  // Enable Timer0 Overflow Interrupt
+}
+
+
+void setup_int0() {
+    GIMSK |= (1 << INT0);    // Enable INT0 interrupt
+}
+
+
+void sleep() {
+	TIMSK0 &= ~(1 << TOIE0);  // Disable Timer0 Overflow Interrupt
+	PORTB &= ~(1 << PINB0);   // Virtual ground off
+	GIMSK |= (1 << INT0);   // Enable INT0 interrupt
+
+	sleep_mode();
+	_delay_ms(800);
+
+	GIMSK &= ~(1 << INT0);  // Disable INT0 interrupt
+	PORTB |= 1 << PINB0;    // Virtual ground on
+	TIMSK0 = (1 << TOIE0);  // Enable Timer0 Overflow Interrupt
+	duration = 0;
+}
+
+
+void quarter_square(const uint16_t period_samples, const uint16_t duration_samples, const uint8_t pin) {
 	if ((duration_samples % period_samples) < (period_samples >> 2)) {
 		PORTB |= 1 << pin;
 	}
@@ -118,11 +77,14 @@ void quarter_square(const uint16_t period_samples, const uint16_t duration_sampl
 
 
 int main() {
-	DDRB |= (1 << PINB2) | (1 << PINB3) | (1 << PINB4);  // Set PB2 PB3 PB4 as output
-	_delay_ms(500);
+	//_delay_ms(3000);
+	setup_sleep();
+	setup_pins();
+	setup_int0();
 	setup_timer0();
-	
-	
+
+	sei();  // Enable global interrupts
+
 	while (1) {
 		if (duration < 6244){
 			quarter_square(42, duration, PINB2);
@@ -145,7 +107,7 @@ int main() {
 			quarter_square(89, duration, PINB4);
 		}
 		else {
-			TIMSK0 &= ~(1 << TOIE0); // Disable Timer0 Overflow Interrupt
+			sleep();
 		}
 	}
 }
@@ -153,4 +115,9 @@ int main() {
 
 ISR(TIM0_OVF_vect) {
 	duration++;
+}
+
+
+ISR(PCINT0_vect) {
+	duration = 0;
 }
