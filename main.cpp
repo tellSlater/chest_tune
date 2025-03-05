dcbcbca12796e3def493dc2ce5d1c0b26d6b67c9
Ilias Chatziefstathiou <ilias.chatziefstathiou@inventyou.se>
2025-03-05 04:35:10 +0100

    3pin_square_final

--- main.cpp
+++ main.cpp
@@ -23,7 +23,10 @@
 
 
 volatile uint16_t duration = 0xffff;
-volatile uint8_t darkness = 0;
+volatile uint8_t note1_samples = 0;
+volatile uint8_t note2_samples = 0;
+volatile uint8_t note3_samples = 0;
+volatile uint8_t darkness = 6;
 
 
 void setup_pins(){
@@ -66,7 +69,7 @@ void setup_watchdog() {
 void sleep() {
 	TIMSK0 &= ~(1 << TOIE0);  // Disable Timer0 Overflow Interrupt
 	PORTB &= ~(1 << PINB0);   // Virtual ground off
-	GIMSK |= (1 << PCIE);     // Enable pin change interrupt
+	//GIMSK |= (1 << PCIE);     // Enable pin change interrupt
 	PORTB &= ~(1 << PINB0);   // Disconnect virtual ground
 
 	while (duration) sleep_mode();
@@ -79,12 +82,15 @@ void sleep() {
 }
 
 
-void quarter_square(const uint16_t period_samples, const uint16_t duration_samples, const uint8_t pin) {
-	if ((duration_samples % period_samples) < (period_samples >> 2)) {
+void quarter_square(const uint16_t note_period_samples, volatile uint8_t* sample_counter_ptr, const uint8_t pin) {
+	if (*sample_counter_ptr < (note_period_samples >> 2)) {
 		PORTB |= 1 << pin;
 	}
-	else {
+	else if (*sample_counter_ptr < (note_period_samples)) {
 		PORTB &= ~(1 << pin);
+	}
+	else {
+		*sample_counter_ptr = 0;
 	}
 }
 
@@ -110,29 +116,29 @@ int main() {
 	setup_pins();
 	setup_pcint();
 	setup_timer0();
-	dbg(5);
+	// dbg(5);
 	sei();  // Enable global interrupts
 
 	while (1) {
 		if (duration < 6244){
-			quarter_square(42, duration, PINB2);
-			quarter_square(71, duration, PINB3);
-			quarter_square(106, duration, PINB4);
+			quarter_square(42, &note1_samples, PINB2);
+			quarter_square(71, &note2_samples, PINB3);
+			quarter_square(106, &note3_samples, PINB4);
 		}
 		else if (duration < 2*6244){
-			quarter_square(40, duration - 6244, PINB2);
-			quarter_square(67, duration, PINB3);
-			quarter_square(100, duration, PINB4);
+			quarter_square(40, &note1_samples, PINB2);
+			quarter_square(67, &note2_samples, PINB3);
+			quarter_square(100, &note3_samples, PINB4);
 		}
 		else if (duration < 3*6244){
-			quarter_square(38, duration - 2*6244, PINB2);
-			quarter_square(63, duration, PINB3);
-			quarter_square(94, duration, PINB4);
+			quarter_square(38, &note1_samples, PINB2);
+			quarter_square(63, &note2_samples, PINB3);
+			quarter_square(94, &note3_samples, PINB4);
 		}
 		else if (duration < (8*6244U)){
-			quarter_square(35, duration - 3*6244, PINB2);
-			quarter_square(60, duration, PINB3);
-			quarter_square(89, duration, PINB4);
+			quarter_square(35, &note1_samples, PINB2);
+			quarter_square(60, &note2_samples, PINB3);
+			quarter_square(89, &note3_samples, PINB4);
 		}
 		else {
 			sleep();
@@ -141,26 +147,29 @@ int main() {
 }
 
 
-ISR(TIM0_OVF_vect) {
+ISR(TIM0_OVF_vect) {  // Executes at 37500Hz when chip is not sleeping
 	duration++;
+	note1_samples++;
+	note2_samples++;
+	note3_samples++;
 }
 
 
-ISR(PCINT0_vect) {
-	_delay_ms(20);
-	//PORTB ^= 1 << PINB0;
-	if (darkness >= 3) {
-		duration = 0;
-		darkness = 0;
-	}
-	GIFR |= (1 << PCIF);  // Clear the pin change interrupt flag
+ISR(PCINT0_vect) {         // Executes on light change
+	duration = 0;
+	darkness = 0;
+	//GIMSK &= ~(1 << PCIE);  // Disable pin change interrupt
 }
 
 
-ISR (WDT_vect) {  //Wake from sleep and check darkness once every 8sec
+ISR (WDT_vect) {							     //Wake from sleep and check darkness once every 8sec
 	if (PINB & 1 << PINB1) {
 		if (darkness < 0xff) darkness++;
+		if (darkness >=0) GIMSK |= (1 << PCIE);  // Enable pin change interrupt
 	}
-	else darkness = 0;
+	else {
+		darkness = 0;
+		GIMSK &= ~(1 << PCIE);                   // Disable pin change interrupt
+    }
 }
 
